6818304fc6b36fed9bd45546ea12a88454744386
Vardan <vardan.vanyan@gmail.com>
2024-10-29 15:26:22 -0700

    added created at, now the user can see when they created the todo

--- src/components/Todo/TodoComponent.tsx
+++ src/components/Todo/TodoComponent.tsx
@@ -7,12 +7,30 @@ type TodoPropTypes = {
   id: string;
   title: string;
   completed: boolean;
+  createdAt: number;
 };
 
-export default function TodoComponent({ id, title, completed }: TodoPropTypes) {
+export default function TodoComponent({
+  id,
+  title,
+  completed,
+  createdAt,
+}: TodoPropTypes) {
   const [editing, setEditing] = useState<boolean>(false);
 
   const { completeTodo, deleteTodo } = useTodos();
+
+  const formatDateTime = (date: number): string => {
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+    });
+    return formatter.format(date);
+  };
 
   return (
     <li
@@ -20,32 +38,35 @@ export default function TodoComponent({ id, title, completed }: TodoPropTypes) {
       className={`${styles.todo} ${completed ? styles.completed : ''}`}
     >
       {editing ? (
-        <>
-          <EditingForm id={id} title={title} setEditing={setEditing} />
-        </>
+        <EditingForm id={id} title={title} setEditing={setEditing} />
       ) : (
         <>
-          <label className={styles.checkboxContainer}>
-            <input
-              type='checkbox'
-              checked={completed}
-              onChange={() => completeTodo(id)}
-              className={styles.checkbox}
-            />
-            <span className={styles.text}>{title}</span>
-          </label>
-          <button
-            className={styles.editButton}
-            onClick={() => setEditing(true)}
-          >
-            Edit
-          </button>
-          <button
-            className={styles.deleteButton}
-            onClick={() => deleteTodo(id)}
-          >
-            Delete
-          </button>
+          <div className={styles.todoContent}>
+            <label className={styles.checkboxContainer}>
+              <input
+                type='checkbox'
+                checked={completed}
+                onChange={() => completeTodo(id)}
+                className={styles.checkbox}
+              />
+              <span className={styles.text}>{title}</span>
+            </label>
+            <p className={styles.createdAt}>{formatDateTime(createdAt)}</p>
+          </div>
+          <div className={styles.buttonContainer}>
+            <button
+              className={styles.editButton}
+              onClick={() => setEditing(true)}
+            >
+              Edit
+            </button>
+            <button
+              className={styles.deleteButton}
+              onClick={() => deleteTodo(id)}
+            >
+              Delete
+            </button>
+          </div>
         </>
       )}
     </li>
