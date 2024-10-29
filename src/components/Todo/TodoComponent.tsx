abd66f5e7885cf5219c9a1a91807e7e4e239f55a
Vardan <vardan.vanyan@gmail.com>
2024-10-29 14:07:07 -0700

    added edit mode, where the user can now edit their past todos

--- src/components/Todo/TodoComponent.tsx
+++ src/components/Todo/TodoComponent.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTodos } from '../../contexts/TodosContext';
 import styles from './Todo.module.css';
 
@@ -8,21 +9,60 @@ type TodoPropTypes = {
 };
 
 export default function TodoComponent({ id, title, completed }: TodoPropTypes) {
-  const { completeTodo } = useTodos();
+  const [editing, setEditing] = useState<boolean>(false);
+
+  const { completeTodo, editTodo } = useTodos();
+
+  function submitEdit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const newTodo = formData.get('edit') as string;
+    editTodo(newTodo, id);
+    setEditing(false);
+  }
   return (
     <li
       key={id}
       className={`${styles.todo} ${completed ? styles.completed : ''}`}
     >
-      <label className={styles.checkboxContainer}>
-        <input
-          type='checkbox'
-          checked={completed}
-          onChange={() => completeTodo(id)}
-          className={styles.checkbox}
-        />
-        <span className={styles.text}>{title}</span>
-      </label>
+      {editing ? (
+        <>
+          <form onSubmit={submitEdit}>
+            <label className={styles.checkboxContainer}>
+              <input type='text' name='edit' placeholder={title} />
+            </label>
+            <button type='submit' className={styles.editButton}>
+              Confirm
+            </button>
+            <button
+              className={styles.deleteButton}
+              onClick={() => setEditing(false)}
+            >
+              Cancel
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <label className={styles.checkboxContainer}>
+            <input
+              type='checkbox'
+              checked={completed}
+              onChange={() => completeTodo(id)}
+              className={styles.checkbox}
+            />
+            <span className={styles.text}>{title}</span>
+          </label>
+          <button
+            className={styles.editButton}
+            onClick={() => setEditing(true)}
+          >
+            Edit
+          </button>
+          <button className={styles.deleteButton}>Delete</button>
+        </>
+      )}
     </li>
   );
 }
