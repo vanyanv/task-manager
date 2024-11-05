ccaa4a6ecbd132e487fcd195dd8bb175b2e3be8f
Vardan <vardan.vanyan@gmail.com>
2024-11-05 14:46:50 -0800

    the user is now able to see categories when creating a new todo

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,50 +1,70 @@
 import React, { useState } from 'react';
 import styles from './TodoForm.module.css';
 import { useTodos } from '../../hooks/useTodos';
+import { todoCategories } from '../../constants/categories';
 
 export default function TodoForm() {
   const { addTodo } = useTodos();
-  const [errors, setError] = useState<{ input: string }>({ input: '' });
+  const [errors, setError] = useState<{ input: string; category: string }>({
+    input: '',
+    category: '',
+  });
 
   function onSubmit(event: React.FormEvent<HTMLFormElement>): void {
     event.preventDefault();
-    //resetting errors
-    setError({ input: '' });
+    setError({ input: '', category: '' });
     const formData = new FormData(event.currentTarget);
-    const todo: string = formData.get('todo') as string;
-    console.log(todo);
+    const todo = formData.get('todo') as string;
+    const category = formData.get('category') as string;
 
-    //manual validation
-    if (todo == '') {
-      setError({ ...errors, input: 'Please Enter A Todo' });
+    if (!todo) {
+      setError({ ...errors, input: 'Please enter a todo' });
       return;
     }
+    if (!todo) {
+      setError({ ...errors, category: 'Please select a category' });
+      return;
+    }
+
     addTodo(todo);
+
     event.currentTarget.reset();
   }
 
   return (
-    <div className={`${styles.container}`}>
+    <div className={styles.container}>
       <form onSubmit={onSubmit} className={styles.form}>
         <div className={styles.inputGroup}>
           <label htmlFor='todo' className={styles.label}>
             Enter A Todo
           </label>
-          <div className={styles.inputWrapper}>
-            <input
-              id='todo'
-              type='text'
-              name='todo'
-              placeholder='What needs to be done?'
-              className={styles.input}
-              required
-            />
-            {errors.input && <p>{errors.input}</p>}
-            <button type='submit' className={styles.button}>
-              Add Todo
-            </button>
-          </div>
+          <input
+            id='todo'
+            type='text'
+            name='todo'
+            placeholder='What needs to be done?'
+            className={styles.input}
+          />
+          {errors.input && (
+            <p className={styles.errorMessage}>{errors.input}</p>
+          )}
         </div>
+
+        <div className={`${styles.inputGroup} ${styles.selectWrapper}`}>
+          <label htmlFor='categories'>Categories:</label>
+          <select id='categories' name='category' className={styles.select}>
+            <option value=''>Select a Category</option>
+            {todoCategories.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button type='submit' className={styles.button}>
+          Add Todo
+        </button>
       </form>
     </div>
   );
